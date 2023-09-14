e9fca21b80b2a075613266a20ac663ca2e21a4a0
hyohyo814 <sojaisam@gmail.com>
2023-09-14 16:13:31 -0700

    Refactor layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { PageLayout } from "~/components/layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  ) 
 };
 
 export default api.withTRPC(MyApp);
