d0193cbcd5c34991d23fcd556a5a8e699279bb00
hyohyo814 <sojaisam@gmail.com>
2023-09-15 21:43:09 -0700

    Set up main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,24 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
+  let tiles = new Array(4);
+
+  for (let i = 0; i < tiles.length; i++) {
+    tiles[i] = `Item ${i}`
+  }
 
   return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 bg-gray-500">
+      <div className="flex flex-row overflow-x-hidden h-full text-4xl overflow-y-hidden py-16">
+        {tiles.map((v, i) => (
+          <div
+            key={i}
+            className="transition px-24 hover:scale-150 hover:cursor-pointer"
+          >
+            {v} 
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
